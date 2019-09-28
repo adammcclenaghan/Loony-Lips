0242905f341953bb6a617551183a5bea8eecbc05
Adam McClenaghan <adam@mcclenaghan.co.uk>
2019-09-28 11:15:32 +0100

    Make replayable

--- LoonyLips.gd
+++ LoonyLips.gd
@@ -1,21 +1,53 @@
 extends Control
 
+var player_words = []
+var prompts = ["a name", "a noun", "an adverb", "an adjective"]
+var story = "Once upon a time %s watched %s and thought it was the %s %s of the past two decades"
+
+onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
+onready var DisplayText = $VBoxContainer/DisplayText
+onready var ButtonText = $VBoxContainer/HBoxContainer/ButtonText
+
 func _ready():
-	var prompts = ["Someone", "trains", "best", "hobby"]
-	var story = "Once upon a time %s watched %s and thought it was the %s %s of the past two decades"
-	print(story % prompts)
+	DisplayText.text = "Welcome to Loony Lips! :) We're going to tell a story and have a great time! "
+	check_player_words_length()
+	PlayerText.grab_focus()
 	
 	# get_node("DisplayText").text = "This is some text"
 	# $VBoxContainer/DisplayText.text = story % prompts
-	
-
 
 func _on_PlayerText_text_entered(new_text):
-	update_DisplayText(new_text)
+	add_to_player_words()
 	
 func _on_TextureButton_pressed():
-	update_DisplayText($VBoxContainer/HBoxContainer/PlayerText.text)
+	if is_story_done():
+		get_tree().reload_current_scene()
+	else:
+		add_to_player_words()
 
-func update_DisplayText(text):
-	$VBoxContainer/DisplayText.text = text
-	$VBoxContainer/HBoxContainer/PlayerText.clear()
+func is_story_done():
+	return player_words.size() == prompts.size()
+
+func check_player_words_length():
+	if is_story_done():
+		end_game()
+	else:
+		prompt_player()
+
+func tell_story():
+	DisplayText.text = story % player_words
+	
+func end_game():
+	PlayerText.queue_free()
+	tell_story()
+	ButtonText.text = "Again!"
+	
+func prompt_player():
+	DisplayText.text += "May I have " + prompts[player_words.size()] + " please?"
+	PlayerText.grab_focus()
+	
+func add_to_player_words():
+	player_words.append(PlayerText.text)
+	DisplayText.text = ""
+	PlayerText.clear()
+	check_player_words_length()
